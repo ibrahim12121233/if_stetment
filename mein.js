4e2adc88fdd48e332c80b004f1ee0d586c14f093
ibrahim <nsism99@gmail.com>
2023-08-16 15:22:24 +0300

    done

---
+++ mein.js
@@ -0,0 +1,17 @@
+let name  = window.prompt("enter your name");
+let age = window.prompt("enter your age");
+
+if (age <= 18 || name =="ibrahim"){
+      alert("welcome back ibrahim");
+}
+else{
+    alert("you cant log in");
+}
+
+let name_1 =prompt("enter your name")
+if(name_1=="ahmed"){
+    alert("the names are conect")
+}else{
+    alert("the names are not conect")
+    
+}
